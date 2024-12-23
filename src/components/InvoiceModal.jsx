098a66207a0fd79db48de6713514fc26cfa12a53
carloslp <juanca2696@gmail.com>
2024-12-22 23:46:11 -0600

    Update InvoiceModal.jsx

--- src/components/InvoiceModal.jsx
+++ src/components/InvoiceModal.jsx
@@ -13,10 +13,10 @@ const GenerateInvoice = () => {
   html2canvas(document.querySelector("#invoiceCapture")).then((canvas) => {
     const imgData = canvas.toDataURL("image/png", 1.0);
     const pdf = new jsPDF({
-      orientation: "portrait",
-      unit: "pt",
-      format: [612, 792],
-    });
+  orientation: 'p', // portrait
+  unit: 'mm', // unit of measurement
+  format: 'a4' // A4 size
+});
     pdf.internal.scaleFactor = 1;
     const imgProps = pdf.getImageProperties(imgData);
     const pdfWidth = pdf.internal.pageSize.getWidth();
